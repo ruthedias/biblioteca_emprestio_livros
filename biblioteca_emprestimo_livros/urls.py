9460ec1331edab3be44bdb7b071b30dd8e7f74e7
Ruth Ellen <ruthpiloes@gmail.com>
2024-02-15 10:19:10 -0300

    Criação e definição da url de home

--- biblioteca_emprestimo_livros/urls.py
+++ biblioteca_emprestimo_livros/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from emprestimo_livros.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="Página Inicial"),
 ]
